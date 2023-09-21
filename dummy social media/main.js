b9916d60b338a467938f83f1fa944c79a3f7e218
Simon O'Shea <osheas1@wit.edu>
2023-09-21 16:52:15 -0400

    files laid out, js begun

--- dummy social media/main.js
+++ dummy social media/main.js
@@ -2,3 +2,27 @@ let form = document.getElementById("form");
 let input = document.getElementById("input");
 let msg = document.getElementById("msg");
 let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e) => 
+{
+    e.preventDefault();
+    console.log("submit clicked");
+
+    formValidation();
+})
+
+let formValidation = () =>
+{
+    
+    if(input.value.trim() == "")
+    {
+        msg.innerHTML = "empty post not permitted"
+        console.log("failed upload. blank post.");
+    }
+    else
+    {
+        console.log("successful post");
+        msg.innerHTML = "";
+    }
+
+};
