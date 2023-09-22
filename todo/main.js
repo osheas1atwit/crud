37a87c4a940ef42589ae0afac004dd0aa5b7b686
Simon O'Shea <osheas1@wit.edu>
2023-09-22 10:43:07 -0400

    started mini todo app

---
+++ todo/main.js
@@ -0,0 +1,29 @@
+let form = document.getElementById("form");
+let msg = document.getElementById("msg");
+
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textArea = document.getElementById("textArea");
+
+let add = document.getElementById("add");
+let tasts = document.getElementById("tasks");
+
+form.addEventListener = (e) => 
+{
+    e.preventDefault();
+    formValidation();
+};
+
+
+
+let formValidation = () =>
+{
+    if(textArea.value === "")
+    {
+
+    }
+    else
+    {
+        
+    }
+};
