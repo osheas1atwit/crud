2638a1567c93c98e129106ab2aa08d6002152ce7
Simon O'Shea <osheas1@wit.edu>
2023-09-22 13:30:43 -0400

    todo done, very basic and has bugs

--- todo/main.js
+++ todo/main.js
@@ -8,22 +8,111 @@ let textArea = document.getElementById("textArea");
 let add = document.getElementById("add");
 let tasts = document.getElementById("tasks");
 
-form.addEventListener = (e) => 
+form.addEventListener("submit", (e) => 
 {
     e.preventDefault();
     formValidation();
-};
+});
 
 
 
 let formValidation = () =>
 {
-    if(textArea.value === "")
+    if(textInput.value === "")
     {
-
+        console.log("failed upload- no text");
+        msg.innerHTML = "task cannot be blank!";
     }
     else
     {
+        console.log("success!");
+        msg.innerHTML = "";
+        acceptData();
         
+        // will only make 'submit' button dismiss if successfull
+        // (rather than putting this attribute on the HTML)
+        add.setAttribute("data-bs-dismiss", "modal");
+        add.click();
+        
+        (() => 
+        {
+            add.setAttribute("data-bs-dismiss", "");
+        })();
     }
 };
+
+let data = [];
+
+let acceptData = () => 
+{
+    data.push
+    ({
+        text: textInput.value,
+        date: dateInput.value,
+        description: textArea.value
+    });
+
+    localStorage.setItem("data", JSON.stringify(data));
+    createTasks();
+
+    console.log(data);
+}
+
+let createTasks = () =>
+{
+    tasks.innerHTML = "";
+    
+    data.map((x, y) => 
+    {
+        return (tasks.innerHTML += 
+        `
+            <div id=${y}>
+                <span class="fw-bold">${x.text}</span>
+                <span class="small text-secondary">${x.date}</span>
+                <p>${x.description}</p>
+
+                <span class="options">
+                    <i onclick="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+                    <i onclick="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+                </span>
+            </div>
+        `);
+    });
+
+    resetForm();
+};
+
+let resetForm = () =>
+{
+    textInput.value = "";
+    dateInput.value = "";
+    textArea.value = "";
+};
+
+let deleteTask = (e) =>
+{
+    e.parentElement.parentElement.remove();
+
+    data.splice(e.parentElement.parentElement.id, 1);
+
+    localStorage.setItem("data", JSON.stringify(data));
+    HTMLFormControlsCollection.log(data);
+};
+
+let editTask = (e) =>
+{
+    let selectedTask = e.parentElement.parentElement;
+
+    textInput.value = selectedTask.children[0].innerHTML;
+    dateInput.value = selectedTask.children[1].innerHTML;
+    textArea.value  = selectedTask.children[2].innerHTML;
+
+    deleteTask(e);
+}
+
+(() => {
+    data = JSON.parse(localStorage.getItem("data")) || [];
+    console.log(data);
+    createTasks();
+  })();
+  
